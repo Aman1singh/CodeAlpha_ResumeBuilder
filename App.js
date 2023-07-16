ed04e67be290ec255b4fa5f0fe0ad06901ce5a4b
Aman1singh <119617619+Aman1singh@users.noreply.github.com>
2023-07-16 19:52:11 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from"./component/Header/Header";
+import Body from "./component/Body/Body";
+import " ./App.css";
+function App(){
+  return(
+    <div className="App">
+      <Header/>
+      <Body/>
+    </div>
+  )
+}
